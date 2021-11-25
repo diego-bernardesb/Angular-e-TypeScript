7e9fdeb875995eeac679fd166fdae74cfe8cb9df
Diego Bernardes <diegobbabo@gmail.com>
2021-11-25 15:52:26 -0300

    Frist commit

---
+++ app.ts
@@ -0,0 +1,10 @@
+let message:String = 'Hello World!';
+
+let num1: number = 8;
+let num2: number = 17;
+let num3: number = 17;
+
+
+let total: number = num1 + num2;
+
+console.log(message,"hoje é dia " + total + " de novembro!");
---
+++ dist/app.js
@@ -0,0 +1,6 @@
+var message = 'Hello World!';
+var num1 = 8;
+var num2 = 17;
+var num3 = 17;
+var total = num1 + num2;
+console.log(message, "hoje é dia " + total + " de novembro!");
