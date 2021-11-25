6df615945ac89b89edc3629d4d1bfa47a57d5c59
Diego Bernardes <diegobbabo@gmail.com>
2021-11-25 16:38:22 -0300

    functions typescript

--- app.ts
+++ app.ts
@@ -7,4 +7,25 @@ let num3: number = 17;
 
 let total: number = num1 + num2;
 
-console.log(message,"hoje é dia " + total + " de novembro!");
+// console.log(message,"hoje é tiveram " + total + " pedidos.");
+
+//função com chamada de operador ternario
+let totalPedidos = function(metaDePedidos: number):boolean{
+    return metaDePedidos > 30;
+}
+
+let pedidosDoDia: number = 28;
+console.log(`A meta de pedidos do dia foi alcançada: ${totalPedidos(pedidosDoDia)? "Sim": "Não"}`)
+
+//Arrow function
+let pedido = (pedido: string) => console.log(`Gostaria de pedir um ${pedido}, por favor.`)
+pedido("combo especial")
+
+
+//parametros padroes
+function qtd(lanches: number, refrigerantes: number = 0) :number{
+    return lanches + refrigerantes;
+}
+
+console.log(`O pedido contem 5 lanches e 3 refrigerantes, total de ${qtd(5, 3)} itens.`)
+console.log(`O pedido contem 5 lanches e 0 refrigerantes, total de ${qtd(5)} itens.`)
--- dist/app.js
+++ dist/app.js
@@ -3,4 +3,20 @@ var num1 = 8;
 var num2 = 17;
 var num3 = 17;
 var total = num1 + num2;
-console.log(message, "hoje é dia " + total + " de novembro!");
+// console.log(message,"hoje é tiveram " + total + " pedidos.");
+//função com chamada de operador ternario
+var totalPedidos = function (metaDePedidos) {
+    return metaDePedidos > 30;
+};
+var pedidosDoDia = 28;
+console.log("A meta de pedidos do dia foi alcan\u00E7ada: ".concat(totalPedidos(pedidosDoDia) ? "Sim" : "Não"));
+//Arrow function
+var pedido = function (pedido) { return console.log("Gostaria de pedir um ".concat(pedido, ", por favor.")); };
+pedido("combo especial");
+//parametros padroes
+function qtd(lanches, refrigerantes) {
+    if (refrigerantes === void 0) { refrigerantes = 0; }
+    return lanches + refrigerantes;
+}
+console.log("O pedido contem 5 lanches e 3 refrigerantes, total de ".concat(qtd(5, 3), " itens."));
+console.log("O pedido contem 5 lanches e 0 refrigerantes, total de ".concat(qtd(5), " itens."));
